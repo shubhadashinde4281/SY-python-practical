1b640e44889d63b8e503f09d27a919f9e362899b
shubhadashinde4281 <shubhada4280@gmail.com>
2026-07-27 13:26:54 +0530

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,12 @@
+print("===========Trafic Signal Rule===========")
+
+signal=input("Enter The Signal Color:(red/yellow/green)")
+
+if signal=="red":
+    print("action : stop")
+    
+elif signal=="yellow":
+    print("action:wait")
+    
+elif signal=="green":
+    print("action:go")  
