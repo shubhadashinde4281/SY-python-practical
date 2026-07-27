a44b80961366fa15a49ecd9ea0f1634fa2521e25
shubhadashinde4281 <shubhada4280@gmail.com>
2026-07-27 13:57:28 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,14 @@
+name=input("Enter Your Name:")
+print("hellow=",name)
+
+age=int(input("enter your age:"))
+print("your age is=",age)
+
+collagename=input("enter our collagename:")
+print("Your Collage Name is=",collagename)
+
+price=float(input("Enter Your Price:"))
+print("Price=",price)
+
+weight=float(input("Enter Your Weight:"))
+print("Weight=",weight)
