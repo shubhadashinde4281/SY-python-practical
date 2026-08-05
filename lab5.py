aad7fa779de269b087d74133cfd6ecd110d3c641
shubhadashinde4281 <shubhada4280@gmail.com>
2026-08-05 13:48:58 +0530

    Implement expenses tracking with category input
    Added a practical expenses tracking feature with categories.

--- lab5.py
+++ lab5.py
@@ -1,3 +1,5 @@
+Trial Code:
+
 expanses=0.0
 while True:
     value=float(input("Enter A Value:"))
@@ -5,3 +7,43 @@ while True:
         break
     expanses=expanses+value 
     print(expanses)
+-----------------------------------------------------------------------------------------
+
+Practical No.5:
+
+
+print("====Expanses====")
+food=0
+travel=0
+shopping=0
+total=0
+other=0
+
+while True:
+    value=float(input("Enter Your Amount"))
+    if value==-1:
+        break
+
+    category=str(input("Enter a category(food/shopping/travel/others):")).lower()
+
+    if category=="food":
+        food=food+value
+
+    elif category=="shopping":
+        shopping=shopping+value
+    elif category=="travel":
+        ttravel=travel+value
+    elif category=="other":
+        other=other+value
+
+total=food+shopping+travel+other
+       
+
+
+print("====Expenses summary====")
+print("Food:",food)
+print("Travel:",travel)
+print("Shopping:",shopping)
+print("Other",other)
+print("Total",total)
+
