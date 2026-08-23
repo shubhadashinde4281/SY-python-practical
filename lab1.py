5b760147fa41d028e3a0ede3f771e8353ba30345
shubhadashinde4281 <shubhada4280@gmail.com>
2026-08-23 18:49:39 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -4,13 +4,13 @@ name=input("Enter Your Name:")
 print("hellow:",name)
 
 age=int(input("\n Enter your age:"))
-print("your age is:",age)
+print("Your age is:",age)
 
 collagename=input("\n Enter our collage name:")
 print("Your Collage Name is:",collagename)
 
 roll_Number=int(input("\n Enter Your Roll No:"))
-print("Roll No:",roll_Number)
+print("Your Roll No is:",roll_Number)
 
 marks=float(input("\n Enter Your Marks:"))
-print("Yoour Marks is:",marks)
+print("Your Marks is:",marks)
